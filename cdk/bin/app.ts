31e7de548b84964e1bc4080e892b97b8aa56715b
Nobuhiro Kuroiwa <nobubijinesu@gmail.com>
2026-04-26 18:33:54 +0900

    Add cdk script to setup aurora

---
+++ cdk/bin/app.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AuroraStack } from '../lib/aurora-stack';
+
+const app = new cdk.App();
+
+new AuroraStack(app, 'AuroraStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
